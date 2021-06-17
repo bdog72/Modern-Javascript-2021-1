3124d610625644e8f882313cc306ea250b71dbf3
Brian Bycynski <high5bri@hotmail.com>
2021-06-17 02:21:59 -0400

    functions

---
+++ Sections/section_3/scope-2.js
@@ -0,0 +1,17 @@
+//
+//
+
+// let firstName = 'Andrew';
+
+if (true) {
+  // let firstName = 'Mike';
+  // console.log(firstName);
+  if (true) {
+    firstName = 'Jen';
+    console.log(firstName);
+  }
+}
+
+if (true) {
+  console.log(firstName);
+}
