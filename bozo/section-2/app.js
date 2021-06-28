11cf0b49855e150118ba678eac8e0f833bf9d323
Brian Bycynski <high5bri@hotmail.com>
2021-06-28 05:23:21 -0400

    if else statements

--- bozo/section-2/app.js
+++ bozo/section-2/app.js
@@ -2,19 +2,55 @@
 //
 // ************ CODING CHALLENGE #1 *****************
 
-const massMark = 78;
-const heightMark = 1.69;
+// const massMark = 78;
+// const heightMark = 1.69;
 
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const markBMI = massMark / heightMark ** 2;
-const johnBMI = massJohn / heightJohn ** 2;
+// const markBMI = massMark / heightMark ** 2;
+// const johnBMI = massJohn / heightJohn ** 2;
 
-console.log(markBMI.toFixed(2));
-console.log(johnBMI);
+// console.log(markBMI.toFixed(2));
+// console.log(johnBMI);
 
-const markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+
+// **************************************************
+
+// const firstName = 'Brian';
+// const job = 'teacher';
+// const birthYear = 1972;
+// const year = 2037;
+
+// const bozo = "I'm " + firstName;
+// const bozo1 = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+
+// console.log(bozo);
+// console.log(bozo1);
+
+// **************************************************
+
+const age = 15;
+
+if (age >= 18) {
+  console.log('Sarah can get her Driving License');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(
+    `Sarah is not old enough to drive. She has ${yearsLeft} years left before she can drive `
+  );
+}
+
+const birthYear = 1999;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
 
 // **************************************************
