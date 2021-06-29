da903c0f0197b199eef25966668921b9f66b8cd1
Brian Bycynski <high5bri@hotmail.com>
2021-06-29 04:26:21 -0400

    Update app.js

--- bozo/section-2/app.js
+++ bozo/section-2/app.js
@@ -32,25 +32,50 @@
 
 // **************************************************
 
-const age = 15;
+// const age = 15;
 
-if (age >= 18) {
-  console.log('Sarah can get her Driving License');
-} else {
-  const yearsLeft = 18 - age;
-  console.log(
-    `Sarah is not old enough to drive. She has ${yearsLeft} years left before she can drive `
-  );
-}
+// if (age >= 18) {
+//   console.log('Sarah can get her Driving License');
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(
+//     `Sarah is not old enough to drive. She has ${yearsLeft} years left before she can drive `
+//   );
+// }
 
-const birthYear = 1999;
-let century;
-if (birthYear <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
+// const birthYear = 1999;
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
 
-console.log(century);
+// console.log(century);
 
+// **************** CODING CHALLENGE 2 **************
+
+// const massMark = 78;
+// const heightMark = 1.69;
+
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const markBMI = (massMark / heightMark ** 2).toFixed(2);
+// const johnBMI = (massJohn / heightJohn ** 2).toFixed(2);
+
+// console.log(markBMI);
+// console.log(johnBMI);
+
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+
+// if (markBMI > johnBMI) {
+//   console.log(`Marks BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
+// } else {
+//   console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
+// }
+
+// **************************************************
+console.log(123);
 // **************************************************
