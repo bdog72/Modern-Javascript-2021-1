ec757da56cddabb95e71f60529f531f408e018c0
Brian Bycynski <high5bri@hotmail.com>
2021-07-04 04:25:13 -0400

    Update app.js

--- bozo/section-3/app.js
+++ bozo/section-3/app.js
@@ -34,4 +34,22 @@
 
 // **************************************************
 
+// function calcAge1(birthYear) {
+//   return 2021 - birthYear;
+// }
+
+// const age1 = calcAge1(1972);
+// console.log(age1);
+
+// // -------------------------------------
+
+// const calcAge2 = function (birthYear) {
+//   return 2021 - birthYear;
+// };
+
+// const age2 = calcAge2(2000);
+// console.log(age2);
+
+// **************************************************
+
 // **************************************************
