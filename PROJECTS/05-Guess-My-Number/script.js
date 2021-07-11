40274d8451785d992bc5b37f13bcedb9f1b1fadf
Brian Bycynski <high5bri@hotmail.com>
2021-07-11 04:15:46 -0400

    Update script.js

--- PROJECTS/05-Guess-My-Number/script.js
+++ PROJECTS/05-Guess-My-Number/script.js
@@ -2,4 +2,11 @@
 
 //
 //
-console.log(1);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value;
+
+document.querySelector('.guess').value = 72;
