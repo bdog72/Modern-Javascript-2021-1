fe1a95e019ba52b3ecb5b216cb0f78b543e12c30
Brian Bycynski <high5bri@hotmail.com>
2021-07-22 02:34:46 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 //
 //
+'use strict';
 
+const bookings = [];
 
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
 
-console.log(123);
+createBooking('LH123');
+createBooking('LH123', 2, 800);
