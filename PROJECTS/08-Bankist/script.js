2a482791d4513533055dc5d792f9282223669263
Brian Bycynski <high5bri@hotmail.com>
2021-07-28 02:40:06 -0400

    Update script.js

--- PROJECTS/08-Bankist/script.js
+++ PROJECTS/08-Bankist/script.js
@@ -88,12 +88,31 @@ const displayMovements = function (movements) {
 displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // LECTURES
+/////////////////////////////////////////////////
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogs);
+
+//   dogs.forEach(function (dog, i) {
+//     if (dog >= 3) {
+//       console.log(`Dog ${i + 1} is an adult, and is ${dog} years old `);
+//     } else {
+//       console.log(`Dog ${i + 1} is an puppy, and is ${dog} years old `);
+//     }
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
 // let arr = ['a', 'b', 'c', 'd', 'e'];
 
