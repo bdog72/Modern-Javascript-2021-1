55a24c0c85455ed2f52c2bb475df718132c8e749
Brian Bycynski <high5bri@hotmail.com>
2021-08-11 03:08:31 -0400

    working on tabbed component

--- PROJECTS/09-Bankist_2/script.js
+++ PROJECTS/09-Bankist_2/script.js
@@ -4,12 +4,17 @@
 'use strict';
 
 ///////////////////////////////////////
+
 // Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const buttonScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -35,27 +40,88 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-///////////////////////////////////////////////////
-
-const buttonScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 buttonScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
 ///////////////////////////////////////////////////
+// Page Navigation
+
+// document.querySelectorAll('.nav__link').forEach(el => {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+///////////////////////////////////////////////////
 
 const h1 = document.querySelector('h1');
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
 
-const alertH1 = function (e) {
-  alert('addEventListener: Great you are reading the headings');
-};
+// h1.firstElementChild.style.color = '#fff';
+// h1.lastElementChild.style.color = 'blue';
 
-h1.addEventListener('mouseenter', alertH1);
+// h1.closest('.header').style.background = 'var(--color-primary-darker)';
 
-setTimeout(() => {
-  h1.removeEventListener('mouseenter', alertH1);
-}, 5000);
+// console.log(h1.firstChild);
+// console.log(h1.firstElementChild);
+
+///////////////////////////////////////////////////
+
+// const ranNum = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColor = () =>
+//   `rgb(${ranNum(0, 255)},${ranNum(0, 255)},${ranNum(0, 255)})`;
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   // e.stopPropagation();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   // e.stopPropagation();
+// });
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav__link').addEventListener(
+//   ('click',
+//   e => {
+//     console.log(e);
+//   })
+// );
+
+// console.log(randomColor());
+
+///////////////////////////////////////////////////
+
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great you are reading the headings');
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => {
+//   h1.removeEventListener('mouseenter', alertH1);
+// }, 5000);
 
 ///////////////////////////////////////////////////
